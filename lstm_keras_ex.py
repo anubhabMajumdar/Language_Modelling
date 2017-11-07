ad366053f9a2dbff9f76ba9a71b18371334d7f65
Anubhab <amajumd@ncsu.edu>
2017-11-07 10:37:15 -0500

    added model

--- lstm_keras_ex.py
+++ lstm_keras_ex.py
@@ -64,8 +64,8 @@ tokenizer.fit_on_texts([data])
 encoded = tokenizer.texts_to_sequences([data])[0]
 # retrieve vocabulary size
 history = 10
-units = 100
-epochs = 200
+units = 200
+epochs = 100
 vocab_size = len(tokenizer.word_index) + 1
 print('Vocabulary Size: %d' % vocab_size)
 # encode 2 words -> 1 word
